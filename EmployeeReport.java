8cbcf67942fc04063134c276ccdf4a55e2a9f37e
Adityachess <adityadone9398@gmail.com>
2021-09-26 13:18:41 +0530

    ADD: Welocme Message

---
+++ EmployeeReport.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.empreport;
+
+public class EmployeeReport {
+	
+	void Welcome(){
+		System.out.println("Welcome to Employee Wage Computation Programme");
+	}
+
+	public static void main(String[] args) {
+		EmployeeReport obj = new EmployeeReport();
+		obj.Welcome();
+
+	}
+
+}
