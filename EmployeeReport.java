d84a9d03360050888b812ddedc4aeab85551ccd3
Adityachess <adityadone9398@gmail.com>
2021-09-26 13:35:00 +0530

    ADD:Checking Employee Attendance

--- EmployeeReport.java
+++ EmployeeReport.java
@@ -1,14 +1,31 @@
 package com.bridgelabz.empreport;
 
+import java.util.Random;
+
 public class EmployeeReport {
 	
 	void Welcome(){
 		System.out.println("Welcome to Employee Wage Computation Programme");
 	}
+	
+	void employeeAttendance() {
+
+		Random ran = new Random();
+		int empCheck= ran.nextInt(2);
+		System.out.println(empCheck);
+		if(empCheck==0) {
+			System.out.println("Employee is Present");
+		}
+		else {	
+			System.out.println("Employee is Absent");
+		}
+
+	}
 
 	public static void main(String[] args) {
 		EmployeeReport obj = new EmployeeReport();
 		obj.Welcome();
+		obj.employeeAttendance();
 
 	}
 
