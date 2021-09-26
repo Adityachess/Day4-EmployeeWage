cb4ff55a9fc869de6dbb0f72399f4292589932a3
Adityachess <adityadone9398@gmail.com>
2021-09-26 13:55:11 +0530

    Calculating daily wage of employee

--- EmployeeReport.java
+++ EmployeeReport.java
@@ -3,29 +3,41 @@ package com.bridgelabz.empreport;
 import java.util.Random;
 
 public class EmployeeReport {
-	
-	void Welcome(){
+
+	int wageperhr = 20;
+	int dailyhrs = 8;
+	int dailywage;
+
+	void Welcome() {
 		System.out.println("Welcome to Employee Wage Computation Programme");
 	}
-	
+
 	void employeeAttendance() {
 
 		Random ran = new Random();
-		int empCheck= ran.nextInt(2);
+		int empCheck = ran.nextInt(2);
+
 		System.out.println(empCheck);
-		if(empCheck==0) {
+
+		if (empCheck == 0) {
 			System.out.println("Employee is Present");
 		}
-		else {	
+		else {
 			System.out.println("Employee is Absent");
 		}
+	}
 
+	void dailyWage() {
+
+		dailywage = wageperhr * dailyhrs;
+		System.out.println("Employee Daily wage = " + dailywage);
 	}
 
 	public static void main(String[] args) {
 		EmployeeReport obj = new EmployeeReport();
 		obj.Welcome();
 		obj.employeeAttendance();
+		obj.dailyWage();
 
 	}
 
